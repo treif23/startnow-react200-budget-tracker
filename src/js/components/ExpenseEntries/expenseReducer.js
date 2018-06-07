0f7e9c31ccf7e9aa7e9097c63301d93e800e2837
Tommy <treif23@github.com>
2018-06-07 14:32:40 -0700

    update 6/7/18

--- src/js/components/ExpenseEntries/expenseReducer.js
+++ src/js/components/ExpenseEntries/expenseReducer.js
@@ -4,9 +4,42 @@ const defaultState = {
     lineItems: []
 };
 
-export default function ExpenseReducer(state = defaultState, action){
-    // the `state = defaultState` above is new ES6 syntax
-  // for defining a default value on a parameter
+export default function ExpenseReducer(state = defaultState, action) {
+    const { type, payload } = action;
+
+    switch (type) {
+        //here in the case of the update description action
+        case 'UPDATE_EXPENSE_DESCRIPTION': {
+            //we'll return an object
+            return {
+                //with all the previous state
+                ...state,
+                //but overwriting the description
+                description: payload.description
+            };
+        }
+
+        case 'UPDATE_EXPENSE_AMOUNT':{
+            return {
+                ...state,
+                amount: payload.amount
+            };
+        }
+
+        case 'ADD_EXPENSE':{
+            const {description, amount} = action.payload;
+            return{
+                description : '',
+                action: '',
+                lineItems:[
+                    //here we have all the previous line items
+                    ...state.lineItems,
+                    //plus a new object
+                    {description, amount}
+                ]
+            };
+        }
+    }
 
     return state;
 }
--- src/js/components/IncomeEntries/incomeReducer.js
+++ src/js/components/IncomeEntries/incomeReducer.js
@@ -5,8 +5,41 @@ const defaultState = {
 };
 
 export default function IncomeReducer(state = defaultState, action){
-    // the `state = defaultState` above is new ES6 syntax
-  // for defining a default value on a parameter
+    const { type, payload } = action;
+
+    switch (type) {
+        //here in the case of the update description action
+        case 'UPDATE_INCOME_DESCRIPTION': {
+            //we'll return an object
+            return {
+                //with all the previous state
+                ...state,
+                //but overwriting the description
+                description: payload.description
+            };
+        }
+
+        case 'UPDATE_INCOME_AMOUNT':{
+            return {
+                ...state,
+                amount: payload.amount
+            };
+        }
+
+        case 'ADD_INCOME':{
+            const {description, amount} = action.payload;
+            return{
+                description : '',
+                action: '',
+                lineItems:[
+                    //here we have all the previous line items
+                    ...state.lineItems,
+                    //plus a new object
+                    {description, amount}
+                ]
+            };
+        }
+    }
 
     return state;
 }
